ab3c3ea8288d2ce32059f40678e743abf5e81fbd
yari-mattei <yari.mattei@gmail.com>
2015-09-17 11:51:43 +0200

    Added Date instead of static text

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+  
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
@@ -10,19 +11,25 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  static char bufferTime[] = "00:00";
+  static char bufferDate[] = "dd/mm/yyyy";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    strftime(bufferTime, sizeof("00:00"), "%H:%M", tick_time);
   } else {
     // Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+    strftime(bufferTime, sizeof("00:00"), "%I:%M", tick_time);
   }
-
+  
+  // Write the current date into the buffer
+  strftime(bufferDate, sizeof("dd/mm/yyyy"), "%d/%m/%Y", tick_time);
+  
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_time_layer, bufferTime);
+  // Display this date on the TextLayer
+  text_layer_set_text(s_name_layer, bufferDate);
 }
 
 
@@ -30,8 +37,8 @@ static void main_window_load(Window *window) {
   // Create name TextLayer
   s_name_layer = text_layer_create(GRect(0, 0, 144, 50));
   text_layer_set_background_color(s_name_layer, GColorClear);
-  text_layer_set_text_color(s_name_layer, GColorBlack);
-  text_layer_set_text(s_name_layer, "YaroX");
+  text_layer_set_text_color(s_name_layer, GColorGreen);
+  //text_layer_set_text(s_name_layer, "YaroX");
   
   // Improve the name TextLayer layout to be more like a watchface
   text_layer_set_font(s_name_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
@@ -40,7 +47,7 @@ static void main_window_load(Window *window) {
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorGreen);
 
   // Improve the time TextLayer layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -54,6 +61,7 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+    text_layer_destroy(s_name_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -72,6 +80,8 @@ static void init() {
     .unload = main_window_unload
   });
 
+  window_set_background_color(s_main_window, GColorBlue);
+  
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
