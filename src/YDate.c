956dc76d7ed6ef895df20a689965f9f3d9160f6d
yari-mattei <yari.mattei@gmail.com>
2015-09-27 21:20:26 +0200

    added day name

--- src/YDate.c
+++ src/YDate.c
@@ -7,8 +7,6 @@ void initializeYDate(){
   text_layer_set_background_color(s_day_layer, GColorWhite);
   text_layer_set_text_color(s_day_layer, GColorBlack);
   
-  //text_layer_set_text(s_day_layer, "DAY");
-  
   // Improve the day TextLayer layout
   text_layer_set_font(s_day_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(s_day_layer, GTextAlignmentLeft);
@@ -31,17 +29,28 @@ void initializeYDate(){
   text_layer_set_font(s_year_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment(s_year_layer, GTextAlignmentLeft);
   
+   // Create dayName TextLayer
+  s_dayName_layer = text_layer_create(GRect(33, 70, 30, 60));
+  text_layer_set_background_color(s_dayName_layer, GColorWhite);
+  text_layer_set_text_color(s_dayName_layer, GColorBlack);
+  
+  // Improve the dayName TextLayer layout
+  text_layer_set_font(s_dayName_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text_alignment(s_dayName_layer, GTextAlignmentLeft);
+  
   // Create Date Layer
   s_date_layer = layer_create(GRect(0, 0, 144, 168));
   
   layer_add_child(s_date_layer, text_layer_get_layer(s_day_layer));
   layer_add_child(s_date_layer, text_layer_get_layer(s_month_layer));
   layer_add_child(s_date_layer, text_layer_get_layer(s_year_layer));
+  layer_add_child(s_date_layer, text_layer_get_layer(s_dayName_layer));
 }
 void destroyYDate(){
   text_layer_destroy(s_day_layer);
   text_layer_destroy(s_month_layer);
   text_layer_destroy(s_year_layer);
+  text_layer_destroy(s_dayName_layer);
   layer_destroy(s_date_layer);
 }
 
@@ -58,14 +67,26 @@ void updateYDate(struct tm* time){
   static char bufferDay[] = "dd";
   static char bufferMonth[] = "mm";
   static char bufferYear[] = "yyyy";
+  static char bufferDayName[] = "dnany";
+  
+  char bufferDayNameTmp[] = "ddd";
   
   // Write the current date into the buffer
   strftime(bufferDay, sizeof("dd"), "%d", time);
   strftime(bufferMonth, sizeof("mm"), "%m", time);
   strftime(bufferYear, sizeof("yyyy"), "%Y", time);
+  strftime(bufferDayNameTmp, sizeof("ddd"), "%a", time);
+  
+  bufferDayName[0] = bufferDayNameTmp[0];
+  bufferDayName[1] = '\n';
+  bufferDayName[2] = bufferDayNameTmp[1];
+  bufferDayName[3] = '\n';
+  bufferDayName[4] = bufferDayNameTmp[2];
+  
   
   // Display this date on the TextLayer
   text_layer_set_text(s_day_layer, bufferDay);
   text_layer_set_text(s_month_layer, bufferMonth);
   text_layer_set_text(s_year_layer, bufferYear);
+  text_layer_set_text(s_dayName_layer, bufferDayName);
 }
--- src/YDate.h
+++ src/YDate.h
@@ -3,6 +3,7 @@
 static TextLayer *s_day_layer;
 static TextLayer *s_month_layer;
 static TextLayer *s_year_layer;
+static TextLayer *s_dayName_layer;
   
 static Layer *s_date_layer;
 
